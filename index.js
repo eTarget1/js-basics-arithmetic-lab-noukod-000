1da7a9bb858d45f1214f5cf60dcde262abf6e08d
clifford <clifford@noukod.com>
2019-02-21 20:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,3 @@ let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = (Math.random() * 20) + 1
 let randomInteger = Math.floor(randomNumber)
 let randomUserID = let randomUserID = randomInteger + 1000000000
-
